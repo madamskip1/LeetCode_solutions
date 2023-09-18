38aae215401db2013f392d1fc1aadd761a96c1ba
Maciej Adamski <madamskip1@gmail.com>
2023-09-18 17:29:43 +0200

    Problem 190: Reverse Bits. Code, modulo and binary shift

---
+++ 190 - Reverse Bits/modulo-binary_shift.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t reversed = 0;
+        for (int i = 31; i >= 0; --i)
+        {
+            if (n % 2 != 0)
+            {
+                reversed += 1 << i;
+            }
+            n = n >> 1;
+        }
+        return reversed;
+    }
+};
